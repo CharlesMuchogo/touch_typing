cfd3e46abab4a1fcf09cc8ba576ff5338958fe6e
charles <muchpaul2@gmail.com>
2023-08-02 20:15:08 +0300

    Fixed small issue on getting complete method every time I type a correct line

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-	// Read sentences from the file
 	file, err := os.Open("sentences.txt")
 	if err != nil {
 		fmt.Println("Error opening the file:", err)
@@ -23,58 +22,42 @@ func main() {
 		sentences = append(sentences, scanner.Text())
 	}
 
-	// Start the touch typing practice
 	fmt.Println("Welcome to Touch Typing Practice!")
-		
+
 	fmt.Println("Type the sentences below as accurately and quickly as you can.\n")
 
-	// Give the user a moment to prepare
 	time.Sleep(2 * time.Second)
 
-	// Loop through sentences
 	for _, sentence := range sentences {
 		fmt.Println("Sentence to type:")
 		fmt.Println(sentence)
 
-		// Start the timer
 		startTime := time.Now()
 
-		// Get user's input
 		fmt.Print("Your input: ")
 		reader := bufio.NewReader(os.Stdin)
 		userInput, _ := reader.ReadString('\n')
 		userInput = strings.TrimSpace(userInput)
 
-		// Stop the timer
 		endTime := time.Now()
 
-		// Calculate the time taken to type the sentence
 		duration := endTime.Sub(startTime)
 
-		// Calculate typing speed in words per minute (WPM)
 		words := strings.Fields(sentence)
 		wordCount := len(words)
 		seconds := duration.Seconds()
 		typingSpeed := float64(wordCount) / (seconds / 60.0)
 
-		// Compare user's input with the correct sentence
 		if userInput == sentence {
-		
+
 			fmt.Printf("Correct! Well done! Typing Speed: %.2f WPM\n", typingSpeed)
 			time.Sleep(2 * time.Second)
-			fmt.Println("\nCongratulations! You have completed the touch typing practice.")
-						
+
 		} else {
 			fmt.Printf("Incorrect. Try again. Typing Speed: %.2f WPM\n", typingSpeed)
-			
-		}
 
-		// Give the user a moment before showing the next sentence
+		}
 		time.Sleep(2 * time.Second)
 	}
-	
-
-
-
+	fmt.Println("\nCongratulations! You have completed the touch typing practice.")
 }
-
